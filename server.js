da86f566f9585a9dc19557864150e60f31abd0b7
Lucas Nardon <lucaslnardon@gmail.com>
2022-07-28 23:53:45 -0300

    ✨ Add turn lights on and off endpoints

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const YeeDiscovery = require("yeelight-platform").Discovery;
+const YeeDevice = require("yeelight-platform").Device;
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
+
 const discoveryService = new YeeDiscovery();
 const app = express();
+
 app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 let list = [];
 discoveryService.on("started", () => {
@@ -21,8 +31,49 @@ app.get("/getAllLights", (req, res) => {
   res.json(list);
 });
 
-app.get("/getAllLights", (req, res) => {
-  res.json(list);
+app.post("/turnLightOn", (req, res) => {
+  const body = {
+    host: "192.168.0.101",
+    port: "55443",
+    id: "0x000000000456b205",
+  };
+
+  const device = new YeeDevice({ host: body.host, port: body.port });
+  device.connect();
+  device.on("deviceUpdate", (newProps) => {
+    device.disconnect();
+    console.log(newProps);
+  });
+  device.on("connected", () => {
+    device.sendCommand({
+      id: body.id,
+      method: "set_power",
+      params: ["on", "smooth", 300],
+    });
+  });
+});
+
+app.post("/turnLightOff", (req, res) => {
+  const body = {
+    host: "192.168.0.101",
+    port: "55443",
+    id: "0x000000000456b205",
+  };
+
+  const device = new YeeDevice({ host: body.host, port: body.port });
+  device.connect();
+  device.on("deviceUpdate", (newProps) => {
+    console.log(newProps);
+  });
+  device.on("connected", () => {
+    device.sendCommand({
+      id: body.id,
+      method: "set_power",
+      params: ["off", "smooth", 300],
+    });
+  });
+
+  device.disconnect();
 });
 
 app.listen(process.env.REACT_APP_SERVER_PORT || 4321);
